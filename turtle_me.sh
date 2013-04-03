3d4f27ce42661eea3a49eb6dc55edf3d90445afa
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2013-04-03 16:35:41 -0500

    Try to make things bsd friendly.

--- turtle_me.sh
+++ turtle_me.sh
@@ -11,13 +11,12 @@ download_image() {
     fi
 }
 
-system=`uname -o`
+system=`uname`
 pwd=`pwd`
 
 echo "System is $system"
 if [[ $system -eq 'Darwin' ]]; then
     defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
     download_image
-#    $(osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'$pwd$image_path'"')
-    $(osascript -e 'tell application "System Events" to set picture of every desktop to POSIX file "'$pwd$image_path'"')
+    $(osascript -e 'tell application "System Events" to set picture of every desktop to POSIX file "'$pwd/$image_path'"')
 fi
